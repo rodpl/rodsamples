a68d7d3f76729f32f33e0255c18e79d519f86509
dabrowski.daniel@gmail.com <dabrowski.daniel@gmail.com@6e058a51-f14c-0410-ad7a-199489dbeab3>
2008-07-14 14:38:47 +0000

    Initial version

---
+++ MSBuildTutorial/trunk/src/app/MyProject/SimpleClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyProject
+{
+	public class SimpleClass
+	{
+		public string Name { get; private set; }
+		
+		/// <summary>
+		/// Initializes a new instance of the SimpleClass class.
+		/// </summary>
+		public SimpleClass(string name)
+		{
+			Name = name;
+		}
+	}
+}
