c4dd914ab97473117addbf4fcf3f418d3271a901
dabrowski.daniel@gmail.com <dabrowski.daniel@gmail.com@6e058a51-f14c-0410-ad7a-199489dbeab3>
2008-07-16 22:57:58 +0000

    Commit after part 004

--- MSBuildTutorial/trunk/src/test/MyProject.Tests/SimpleClassTests.cs
+++ MSBuildTutorial/trunk/src/test/MyProject.Tests/SimpleClassTests.cs
@@ -1,6 +1,20 @@
-﻿namespace MyProject
+﻿using NUnit.Framework;
+
+namespace MyProject
 {
+	[TestFixture]
 	public class SimpleClassTests
 	{
+		[Test]
+		public void FailTest()
+		{
+			Assert.Fail("Test failed.");
+		}
+
+		[Test]
+		public void PassTest()
+		{
+			Assert.IsTrue(true);
+		}
 	}
 }
