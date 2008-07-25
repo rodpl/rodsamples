1d2f92bcc60f4b7f9a6df3c3c5c35ae703191279
dabrowski.daniel@gmail.com <dabrowski.daniel@gmail.com@6e058a51-f14c-0410-ad7a-199489dbeab3>
2008-07-25 20:16:01 +0000

    Part 006

---
+++ MSBuildTutorial/tags/part006/src/app/MyProject/SimpleClass.cs
@@ -0,0 +1,15 @@
+﻿namespace MyProject
+{
+	public class SimpleClass
+	{
+		public string Name { get; private set; }
+		
+		/// <summary>
+		/// Initializes a new instance of the SimpleClass class.
+		/// </summary>
+		public SimpleClass(string name)
+		{
+			Name = name;
+		}
+	}
+}
