2daf2f3cd6dd2576b6a0f0f9e3e205a0a5147c84
dabrowski.daniel@gmail.com <dabrowski.daniel@gmail.com@6e058a51-f14c-0410-ad7a-199489dbeab3>
2008-07-30 19:34:19 +0000

    Recreate 007

---
+++ MSBuildTutorial/tags/part007/src/test/MyProject.Tests/SimpleClassTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace MyProject
+{
+	[TestFixture]
+	public class SimpleClassTests
+	{
+		[Test]
+		public void FailTest()
+		{
+			Assert.Fail("Test failed.");
+		}
+
+		[Test]
+		public void PassTest()
+		{
+			Assert.IsTrue(true);
+		}
+	}
+}
